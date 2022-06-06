c49f959faa26b573419b30ed963ae68122d4ae4b
evgenykrotov <krotoveann@gmail.com>
2022-06-06 20:01:25 +0300

    new test implementation

--- src/test/java/com/example/exchangerate/GifServiceTest.java
+++ src/test/java/com/example/exchangerate/GifServiceTest.java
@@ -1,38 +1,46 @@
-//package com.example.exchangerate;
-//
-//import com.example.exchangerate.client.GifClient;
-//import com.example.exchangerate.model.Gif;
-//import com.example.exchangerate.service.GifService;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.boot.test.mock.mockito.MockBean;
-//
-//import java.net.URI;
-//
-//import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//import static org.mockito.ArgumentMatchers.any;
-//import static org.mockito.Mockito.when;
-//
-//@SpringBootTest
-//public class GifServiceTest {
-//
-//    @Autowired
-//    GifService gifService;
-//    @MockBean
-//    private GifClient gifClient;
-//
-//    @Test
-//    void urlCreating() {
-//        URI responseUri;
-//        Gif gifModel = new Gif();
-//        String url = "https://media3.giphy.com/media/iJUdMfiwyNdUiAPLPH//giphy.mp4?cid=b5c715b9772563673fcdd55d2875bf67d450fcb71ffff35c&rid=giphy.mp4&ct=g%22";
-//        String tag = "rich";
-//        when(gifClient.getRandomGif(any(), any())).thenReturn(gifModel);
-//        var result = assertDoesNotThrow(gifModel.getUrl());
-//        assertEquals
-//    }
-//
-//
-//}
+package com.example.exchangerate;
+
+import com.example.exchangerate.client.GifClient;
+import com.example.exchangerate.model.Gif;
+import com.example.exchangerate.service.GifService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+public class GifServiceTest {
+
+    @Autowired
+    GifService gifService;
+    @MockBean
+    private GifClient gifClient;
+
+    @Test
+    void getGif() {
+        Gif randomGifResponse = new GifTest();
+        String tag = "rich";
+        var gifResponse = new byte[]{0x12, 0x22, 0x40};
+        when(gifClient.getRandomGif(any(), any()))
+                .thenReturn(randomGifResponse);
+        when(gifClient.getGif(any()))
+                .thenReturn(gifResponse);
+        var result = assertDoesNotThrow(() -> gifService.getGif(tag));
+        assertEquals(gifResponse, result);
+    }
+
+    static class GifTest extends Gif {
+        @Override
+        public String getUrl() {
+            return "giveUrl";
+        }
+
+    }
+
+
+}
